e91f88cafea2c5f76af853bb08945421ed6209a3
seveibar <seveibar@gmail.com>
2025-05-26 11:09:50 -0700

    show correct PipelineDebugger usage

--- website/pages/pipeline/pipeline01.page.tsx
+++ website/pages/pipeline/pipeline01.page.tsx
@@ -4,11 +4,41 @@ import { PipelineDebugger } from "website/components/PipelineDebugger"
 
 export default () => (
   <PipelineDebugger
-    solver={
-      new SchematicLayoutPipelineSolver({
-        // TODO let's make the pipeline debugger accept tscircuit code
-        inputNetlist,
-      })
-    }
+    tscircuitCode={`
+      import { sel } from "tscircuit"
+
+      export default () => (
+        <board routingDisabled>
+          <chip
+            name="U1"
+            footprint="soic6"
+            pinLabels={{
+              pin1: "IN",
+              pin2: "EN",
+              pin3: "GND",
+              pin6: "OUT"
+            }}
+            connections={{ pin1: sel.net.IN, pin2: sel.net.EN, pin3: sel.net.GND }}
+          />
+          <resistor
+            name="R3"
+            resistance="1k"
+            footprint="0402"
+            connections={{ pin1: sel.net.EN, pin2: sel.net.VIN }}
+          />
+          <capacitor
+            name="C1"
+            capacitance="1uF"
+            footprint="0402"
+            connections={{ pin1: sel.net.VIN, pin2: sel.net.GND }}
+          />
+          <capacitor
+            name="C2"
+            capacitance="1uF"
+            footprint="0402"
+            connections={{pin1: sel.net.V3_3, pin2: sel.net.GND}}
+          />
+        </board> 
+      `}
   />
 )
