a8db28555e7461ae239ece4e1c9db9abf231b55c
seveibar <seveibar@gmail.com>
2025-06-06 19:45:54 -0700

    prep corpus

---
+++ website/pages/corpus/corpus01.page.tsx
@@ -0,0 +1,26 @@
+import { PipelineDebugger } from "website/components/PipelineDebugger"
+
+export const corpus1Code = `
+export default () => (
+  <board width="10mm" height="10mm">
+    <chip
+      name="U1"
+      footprint="soic4"
+      connections={{
+        pin1: ".R1 .pin1",
+      }}
+    />
+    <resistor
+      resistance="1k"
+      footprint="0402"
+      name="R1"
+      connections={{
+        pin2: "net.GND",
+      }}
+    />
+  </board>
+)
+
+`
+
+export default () => <PipelineDebugger tscircuitCode={corpus1Code} />
