eaf4c023990eaf224af195db49a552cddadf60b6
Severin Ibarluzea <seve700@gmail.com>
2025-06-14 22:03:15 -0700

    Add corpus02 example and update tscircuit8 snapshot

--- lib/corpus-vfs.ts
+++ lib/corpus-vfs.ts
@@ -1,5 +1,7 @@
 import * as _corpus2025_05_03_abcd1234_json from "./../corpus/corpus2025-05-03-abcd1234.json"
+import * as _corpus2025_05_04_efgh5678_json from "./../corpus/corpus2025-05-04-efgh5678.json"
 
 export default {
   "corpus2025-05-03-abcd1234.json": _corpus2025_05_03_abcd1234_json,
+  "corpus2025-05-04-efgh5678.json": _corpus2025_05_04_efgh5678_json,
 }
